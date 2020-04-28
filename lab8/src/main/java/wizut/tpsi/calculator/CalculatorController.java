b7c0048539b15b531e2eaa35c3550d21eb847320
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-04-28 17:54:51 +0200

    Injected dependencies

--- lab8/src/main/java/wizut/tpsi/calculator/CalculatorController.java
+++ lab8/src/main/java/wizut/tpsi/calculator/CalculatorController.java
@@ -1,12 +1,15 @@
 package wizut.tpsi.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CalculatorController {
     
+    @Autowired
     private CalculatorService calculatorService;
     
     @RequestMapping("/")
